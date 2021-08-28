580372c78ca360d3f33e3cfcf824ddb9001ebb89
drsnn <drscaliskan@gmail.com>
2021-08-28 19:51:11 +0300

    mdd

--- Replaces.js
+++ Replaces.js
@@ -11,7 +11,7 @@
 // @exclude        https://userscripts-mirror.org/scripts/edit/*
 // @exclude        https://userscripts-mirror.org/scripts/edit_src/*
 // @copyright      JoeSimmons
-// @version  20210823203837743171579
+// @version  20210828194949713897639
 // @license        http://creativecommons.org/licenses/by-nc-nd/3.0/us/
 // @downloadURL    https://raw.githubusercontent.com/drsnn/deneme1/master/Replaces.js
 // @updateURL      https://raw.githubusercontent.com/drsnn/deneme1/master/Replaces.js
@@ -69,7 +69,8 @@
 'polisinsats': '(police operation) polisinsats',
 'överstiger': '(exceeds) överstiger',
 'bär på': '(carries on) bär på',
-'påtaglig': '(noticeable) påtaglig',////////////////////////////////////////////////////////
+'påtaglig': '(noticeable) påtaglig',
+'förtvivlade': '(desperate) förtvivlade',////////////////////////////////////////////////////////
     '':''};
 
 
